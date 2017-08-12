63be72193389e54632b07fe6ac7373a8bab01188
Devan <devan.moylan@backyardlion.com>
2017-08-11 19:17:21 -0600

    Create controller method for attractions that allows admin to create a new attraction.

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -1,4 +1,6 @@
 class AttractionsController < ApplicationController
+  before_action :attraction_params, only: [:create]
+
   def show
     @attraction = Attraction.find(params[:id])
   end
@@ -12,6 +14,21 @@ class AttractionsController < ApplicationController
 
   def new
     @attraction = Attraction.new
+  end
+
+  def create
+    @attraction = Attraction.new(attraction_params)
+    if @attraction.save
+      redirect_to attraction_path(@attraction)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def attraction_params
+    params.require(:attraction).permit(:name, :min_height, :happiness_rating, :nausea_rating, :tickets)
   end
 
 end
